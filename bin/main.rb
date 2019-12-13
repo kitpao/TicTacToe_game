3b36e456ba5622fb8a7449417f1140bbc8ec5781
kitpao <kitpao91@gmail.com>
2019-12-13 15:09:42 -0600

    begin user interface milestone

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+puts "Welcome to Tic Tac Toe, type 1 for 'how to play' or 2 to begin game"
+welcome_input = gets.chomp.to_i
+while welcome_input != 1 && welcome_input != 2
+  puts "Please select 1 (instructions) or 2 (begin)"
+  welcome_input = gets.chomp.to_i
+end
+if welcome_input == 1
+  puts "HERE GOES THE INSTRUCTIONS"
+else
+
+end
