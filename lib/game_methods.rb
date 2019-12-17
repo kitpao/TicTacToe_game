cac6765dc713b430cbd026dbb3e8bda9646353f0
Yair fernando <yair.facio11@gmail.com>
2019-12-17 11:56:26 -0600

    Add new possibilites

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -39,7 +39,7 @@ class Player
   end
 
   def winner(selection)
-    possibilities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    possibilities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7]]
     sorted = selection.sort
     possibilities.each do |x|
       if sorted & x == x
