67f186374469ec1889694af72d338ed850100e01
kitpao <kitpao91@gmail.com>
2019-12-20 12:05:27 -0600

    fix linters

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -5,7 +5,7 @@ require './lib/game_methods.rb'
 
 RSpec.describe 'Tic Tac Toe Game' do
   this_game = Game.new
-  let (:board) { this_game.available_board }
+  let(:board) { this_game.available_board }
 
   describe '#next_turn' do
     this_game = Game.new
@@ -21,12 +21,12 @@ RSpec.describe 'Tic Tac Toe Game' do
 
   describe '#update_board' do
     it 'when the board changes with an odd turn' do
-      this_game.update_board(2,3)
+      this_game.update_board(2, 3)
       expect(board).to eq([1, 'X', 3, 4, 5, 6, 7, 8, 9])
     end
 
     it 'when the board changes with an even turn' do
-      this_game.update_board(6,4)
+      this_game.update_board(6, 4)
       expect(board).to eq([1, 'X', 3, 4, 5, 'O', 7, 8, 9])
     end
   end
@@ -40,5 +40,4 @@ RSpec.describe 'Tic Tac Toe Game' do
       expect(this_game.checker(1)).to be false
     end
   end
-
 end
