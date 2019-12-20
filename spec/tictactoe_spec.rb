4811e5dd76028d3ade56f77bc4b34b97ee1b547a
Yair fernando <yair.facio11@gmail.com>
2019-12-20 11:56:43 -0600

    Finished the test units for update_board method

---
+++ spec/tictactoe_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require './lib/player_methods.rb'
+require './lib/game_methods.rb'
+
+RSpec.describe 'Tic Tac Toe Game' do
+  this_game = Game.new
+  let (:board) { this_game.available_board }
+
+  describe '#next_turn' do
+    this_game = Game.new
+
+    it 'when it is an odd turn' do
+      expect(this_game.next_turn(3)).to be false
+    end
+
+    it 'when it is an even' do
+      expect(this_game.next_turn(8)).to be true
+    end
+  end
+
+  describe '#update_board' do
+    it 'when the board changes with an odd turn' do
+      this_game.update_board(2,3)
+      expect(board).to eq([1, 'X', 3, 4, 5, 6, 7, 8, 9])
+    end
+
+    it 'when the board changes with an even turn' do
+      this_game.update_board(6,4)
+      expect(board).to eq([1, 'X', 3, 4, 5, 'O', 7, 8, 9])
+    end
+  end
+end
